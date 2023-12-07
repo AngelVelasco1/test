43b4e292a5f3d99cf3181464aa55e64d0810d6e6
Angel Velasco <angelgg2020@outlook.com>
2023-12-07 15:45:23 -0500

    feat: :lipstick: Imprive rick and morty page ui and ux

--- src/components/Autors.tsx
+++ src/components/Autors.tsx
@@ -21,20 +21,20 @@ export const Autors: any = () => {
     });
   }, []);
   return characters.map((character) => (
-    <a key={character.id} className="group relative block h-64 sm:h-80 lg:h-96 w-full cursor-pointer">
-      <span className="absolute inset-0 border-2 border-dashed border-black"></span>
+    <a key={character.id} className="group relative block h-64 sm:h-80 lg:h-96 w-full cursor-pointer ">
+      <span className="absolute inset-0 border-2 border-dashed rounded-md border-black"></span>
 
-      <div className="relative flex h-full transform items-end border-2 border-black bg-white transition-transform group-hover:-translate-x-2 group-hover:-translate-y-2">
+      <div className="relative flex h-full transform items-end border-2 border-black rounded-md bg-white transition-transform group-hover:-translate-x-2 group-hover:-translate-y-2">
         <div className="m-auto !pt-0 transition-opacity group-hover:absolute group-hover:opacity-0">
          
-           <img src={character.image} alt="" className="rounded-2xl" />
+           <img src={character.image} alt="" className="rounded-2xl w-72" />
         </div>
 
         <div className="absolute   opacity-0 transition-opacity group-hover:relative group-hover:opacity-100 sm:p-6 lg:p-8">
-          <h2 className="mt-4 text-xl font-medium sm:text-2xl">
+          <h2 className="mt-4 text-2xl font-medium  text-[#E9A763]">
             {character.name}
           </h2>
-          <h3 className="mt-4 text-xl font-medium sm:text-2xl">
+          <h3 className=" text-lg font-medium  text-[#343A3F] ">
             {character.species}
           </h3>
         </div>
--- src/views/AutorsView.tsx
+++ src/views/AutorsView.tsx
@@ -2,9 +2,33 @@ import { Autors } from "../components/Autors";
 
 export const AutorsView: React.FC = () => {
   return (
-    <div className="flex flex-row m-auto w-screen p-10 gap-4">
+    <>
+      <h1 className="text-center text-gray-700 mt-8 font-extrabold">Rick and Morty Characters</h1>
+      <a
+  className="inline-block ms-10 rounded-full border-3 border-[#7DA870] bg-[#7DA870]  p-3 text-white hover:bg-transparent hover:text-[#7DA870] focus:outline-none focus:ring active:text-[#7DA870]"
+  href="/"
+>
+  <span className="sr-only"> Go back </span>
 
+  <svg
+    className="h-5 w-5 rotate-180"
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    stroke="currentColor"
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="3"
+      d="M14 5l7 7m0 0l-7 7m7-7H3"
+    />
+  </svg>
+</a>
+
+    <div className="grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2  w-screen p-12 gap-10">
       <Autors />
-    </div>
+    </div></>
+        
   )
 }
